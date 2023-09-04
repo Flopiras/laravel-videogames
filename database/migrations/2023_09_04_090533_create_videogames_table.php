681c910764e2bb2b97e5fc410a4fdb75a81b5341
Flopiras <Flopiras@yahoo.it>
2023-09-04 11:22:46 +0200

    create migrate and seeder Videogame

--- database/migrations/2023_09_04_090533_create_videogames_table.php
+++ database/migrations/2023_09_04_090533_create_videogames_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('videogames', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50)->unique();
+            $table->string('year', 4)->nullable();
+            $table->url('cover')->nullable();
+            $table->text('description')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
--- database/seeders/VideogameSeeder.php
+++ database/seeders/VideogameSeeder.php
@@ -2,16 +2,27 @@
 
 namespace Database\Seeders;
 
+use App\Models\Videogame;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator;
 
 class VideogameSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Generator $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $videogame = new Videogame();
+
+            $videogame->title = $faker->sentence(3);
+            $videogame->year = $faker->year();
+            $videogame->cover = $faker->imageUrl();
+            $videogame->description = $faker->paragraphs(10, true);
+
+            $videogame->save();
+        }
     }
 }

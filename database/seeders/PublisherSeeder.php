50a282b807b9858a58ba8c8b260dd5b19a933482
Flopiras <Flopiras@yahoo.it>
2023-09-08 12:04:45 +0200

    edit seeder

--- database/seeders/PublisherSeeder.php
+++ database/seeders/PublisherSeeder.php
@@ -2,16 +2,25 @@
 
 namespace Database\Seeders;
 
+use App\Models\Publisher;
+use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 
 class PublisherSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Generator $faker): void
     {
-        //
+
+        $publisher = new Publisher();
+
+        $publisher->label = $faker->company();
+        $publisher->color = $faker->hexColor();
+
+        $publisher->save();
     }
 }

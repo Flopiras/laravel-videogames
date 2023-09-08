855fb5e205ea21550ab85b01ea0451641cba3a2a
l3on <l3onwebdev@gmail.com>
2023-09-08 16:19:28 +0200

    added create page

---
+++ resources/views/admin/consoles/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    {{-- Section Header --}}
+    <header class="d-flex justify-content-between align-items-center mt-5">
+        <h2>Create new console</h2>
+        <a href="{{ route('admin.consoles.index') }}" class="btn btn-secondary">Back</a>
+    </header>
+
+    @include('includes.consoles.form')
+@endsection
